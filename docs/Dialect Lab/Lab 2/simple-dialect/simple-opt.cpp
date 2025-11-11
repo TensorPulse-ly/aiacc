16af982361ced03f7c9c50320857a82e2b314d3b
Jabari-He <1421234163@qq.com>
2025-11-11 17:28:35 +0800

    Update simple-opt.cpp

--- docs/Dialect Lab/Lab 2/simple-dialect/simple-opt.cpp
+++ docs/Dialect Lab/Lab 2/simple-dialect/simple-opt.cpp
@@ -9,6 +9,7 @@ int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   
   registry.insert<mlir::func::FuncDialect>();
+  registry.insert<mlir::func::ArithDialect>();
   registry.insert<simple::SimpleDialect>();
   
   // 注册我们的 Pass
